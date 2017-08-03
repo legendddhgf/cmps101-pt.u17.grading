8f1bb2af36b6b352411db02724dedc9ce5f911b1
legendddhgf <legendddhgf@gmail.com>
2017-08-03 01:34:56 -0700

    forgot to change pa4.sh appropriately

--- pa4/pa4.sh
+++ pa4/pa4.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # cmps101-pt.u17 grading
-# usage: pa2.sh
-# (run within your pa2 directory to test your code)
+# usage: pa4.sh
+# (run within your pa4 directory to test your code)
 
-SRCDIR=https://raw.githubusercontent.com/legendddhgf/cmps101-pt.u17.grading/master/pa2
-EXE="pa2-check.sh"
+SRCDIR=https://raw.githubusercontent.com/legendddhgf/cmps101-pt.u17.grading/master/pa4
+EXE="pa4-check.sh"
 SCRIPT=$(mktemp)
 
 curl $SRCDIR/$EXE > $EXE
